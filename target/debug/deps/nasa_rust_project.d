e57f64c609b2f4477b7e2233512ce21ec9d2e574
elvie <elvie@example.com>
2026-03-07 18:30:14 +0800

    chore: remove target and stale dirs from tracking

--- target/debug/deps/nasa_rust_project.d
+++
@@ -1,5 +0,0 @@
-C:\Users\elvie\nasa-rust-project\target\debug\deps\nasa_rust_project.d: src\main.rs
-
-C:\Users\elvie\nasa-rust-project\target\debug\deps\nasa_rust_project.exe: src\main.rs
-
-src\main.rs:
